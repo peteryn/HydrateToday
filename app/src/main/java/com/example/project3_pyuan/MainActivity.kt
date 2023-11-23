62628a013c2b7b165fb45c21705900ab1dbcd73a
Peter Yuan <peter.yuan18@gmail.com>
2023-11-23 12:52:20 -0500

    Add text field in onboarding

--- app/src/main/java/com/example/project3_pyuan/MainActivity.kt
+++ app/src/main/java/com/example/project3_pyuan/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.example.project3_pyuan
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract.Data
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,10 +31,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,11 +46,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.lang.NumberFormatException
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val store = UserStore(this)
+
         setContent {
             Project3pyuanTheme {
                 // A surface container using the 'background' color from the theme
@@ -55,22 +62,22 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 //                    Greeting("Android")
-                    Layout()
+                    val myFlow = store.getAccessToken
+                    val flowValue: String
+                    runBlocking(Dispatchers.IO) {
+                        flowValue = myFlow.first()
+                    }
+                    if (flowValue.isEmpty()) {
+                        PagerAnimateToItem()
+                    } else {
+                        Layout()
+                    }
                 }
             }
         }
 
         val dbHelper: DatabaseOpenHelper = DatabaseOpenHelper(this)
-        val store = UserStore(this)
-        val myFlow = store.getAccessToken
-        val flowValue: String
-//        runBlocking(Dispatchers.IO) {
-//            flowValue = myFlow.first()
-//        }
-//        if (flowValue.isEmpty()) {
-//
-//        }
-   }
+  }
 
     fun getInfo() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
@@ -128,33 +135,11 @@ fun Layout() {
         val store = UserStore(context)
         val tokenText = store.getAccessToken.collectAsState(initial = "")
 
-        val flowValue: String
-        runBlocking(Dispatchers.IO) {
-            flowValue = store.getAccessToken.first()
-        }
+//        val flowValue: String
+//        runBlocking(Dispatchers.IO) {
+//            flowValue = store.getAccessToken.first()
+//        }
 
-        val openAlertDialog = remember { mutableStateOf(flowValue.isEmpty()) }
-//        when {
-//            openAlertDialog.value -> {
-//                AlertDialogExample(
-//                    onDismissRequest = { openAlertDialog.value = true },
-//                    onConfirmation = {
-//                        openAlertDialog.value = false
-//                        Log.w("INFO","Confirmation registered") // Add logic here to handle confirmation.
-//                    },
-//                    dialogTitle = "First Time Setup",
-//                    dialogText = "This is an example of an alert dialog with buttons.",
-//                )
-//            }
-//        }
-//        if (tokenText.value.isEmpty()) {
-//            AlertDialogExample(
-//                onDismissRequest = { },
-//                onConfirmation = { /*TODO*/ },
-//                dialogTitle = "No data",
-//                dialogText = "no data",
-//            )
-//        }
 
         Column(
             modifier = Modifier
@@ -238,14 +223,141 @@ fun AlertDialogExample(
                 Text("Confirm")
             }
         },
-//        dismissButton = {
-//            TextButton(
-//                onClick = {
-//                    onDismissRequest()
-//                }
-//            ) {
-//                Text("Dismiss")
-//            }
-//        }
     )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+fun PagerAnimateToItem() {
+    val context = LocalContext.current
+    val store = UserStore(context)
+    var weight by remember {
+        mutableStateOf(0)
+    }
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+    ){
+        // [START android_compose_layouts_pager_scroll_animate]
+        var currentPage by remember {
+            mutableStateOf(0)
+        };
+        val pagerState = rememberPagerState(0)
+        currentPage = 0
+        val myContext = LocalContext.current
+
+        HorizontalPager(state = pagerState, pageCount = 3) { page ->
+            // Our page content
+            when (page) {
+                0 -> {
+                    weight = page("Test", "test")
+                    Log.w("Weight", weight.toString())
+                }
+                1 -> page("stuff", "stuff")
+                2 -> page("zebra", "test")
+            }
+//            Text(
+//                text = "Page: $page",
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(100.dp)
+//            )
+        }
+
+        // scroll to page
+        val coroutineScope = rememberCoroutineScope()
+        var s by remember {
+            mutableStateOf("Next")
+        }
+        Button(onClick = {
+            if (s == "Finish") {
+                CoroutineScope(Dispatchers.IO).launch {
+                    store.saveToken("Done")
+                }
+                val intent = Intent(myContext, MainActivity::class.java)
+                myContext.startActivity(intent)
+            }
+            else {
+                coroutineScope.launch {
+                    // Call scroll to on pagerState
+                    Log.w("INFO", currentPage.toString())
+                    if (currentPage < 2) {
+                        currentPage++
+                        pagerState.animateScrollToPage(currentPage)
+                    }
+                }
+            }
+        }, modifier = Modifier.align(Alignment.BottomEnd)) {
+            if (currentPage == 2) {
+                s = "Finish"
+            }
+            else {
+                s = "Next"
+            }
+            Text(s);
+        }
+        if (currentPage != 0) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    Log.w("INFO", currentPage.toString())
+                    // Call scroll to on pagerState
+                    if (currentPage > 0) {
+                        currentPage--;
+                        pagerState.animateScrollToPage(currentPage)
+                    }
+                }
+            }, modifier = Modifier.align(Alignment.BottomStart)) {
+                Text("Back")
+            }
+        }
+        // [END android_compose_layouts_pager_scroll_animate]
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun page(heading: String, subtext: String) : Int {
+    val context = LocalContext.current // strange that we have to put it up here
+    var weight by remember {
+        mutableStateOf("")
+    }
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(text = heading)
+        Text(text = subtext)
+        TextField(
+            value = weight,
+            label = {Text("Weight in lbs.")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = {
+                if (it.contains(".") || it.contains("-") || it.contains(" ") || it.contains(",") || it.contains("\n")) {
+                    // do nothing
+                } else {
+                    try {
+                        Log.w("INFO", "In try")
+                        it.toInt()
+                        Log.w("INFO", "Past first check")
+                        weight = it
+                        Log.w("INFO", "Weight should be updated")
+                    } catch (e: NumberFormatException) {
+                        // toast displaying stuff
+                        weight = ""
+                        if (it.isNotEmpty()) {
+                            Toast.makeText(context, "Please enter a smaller weight", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+        )
+    }
+    if (weight.toString().isEmpty()) {
+        return 0
+    }
+
+    return weight.toInt()
 }
