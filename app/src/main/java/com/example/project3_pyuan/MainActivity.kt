8a65d1c388b1d47695c6e16820717ec7318c779e
Peter Yuan <peter.yuan18@gmail.com>
2023-11-26 18:49:14 -0500

    Organize onCreate

--- app/src/main/java/com/example/project3_pyuan/MainActivity.kt
+++ app/src/main/java/com/example/project3_pyuan/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.example.project3_pyuan
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,11 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -34,23 +24,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.project3_pyuan.onboarding.pages.page0
-import com.example.project3_pyuan.onboarding.pages.page1
-import com.example.project3_pyuan.onboarding.pages.page3
 import com.example.project3_pyuan.ui.theme.Project3pyuanTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -65,18 +46,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val store = UserStore(this)
-        val day = store.getDay
-        var dayValue = 0
-        val waterDrunk = store.getTodayWater
-        var waterDrunkValue = 0
-        runBlocking {
-            dayValue = day.first()
-            waterDrunkValue = waterDrunk.first()
-        }
-
-
-
         setContent {
             Project3pyuanTheme {
                 // A surface container using the 'background' color from the theme
@@ -84,28 +53,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val onBoardingStatusFlow = store.getOnboardingStatus
-                    val onboardingStatus: Boolean
+                    val store = UserStore(this)
+
+                    val dayFlow = store.getDay
+                    var dayValue = 0
                     val today = secondsToDay((System.currentTimeMillis()/1000).toInt())
-                    val waterGoal = store.getUserWaterGoal
+
+                    val waterDrunkFlow = store.getTodayWater
+                    var waterDrunkValue = 0
+
+                    val onBoardingStatusFlow = store.getOnboardingStatus
+                    val onboardingStatusValue: Boolean
+
+                    val waterGoalFlow = store.getUserWaterGoal
                     var waterGoalValue = 0
+
                     val currentStreak = store.getCurrentStreak
                     var currentStreakValue = 0
+
                     runBlocking(Dispatchers.IO) {
-                        onboardingStatus = onBoardingStatusFlow.first()
-                        waterGoalValue = waterGoal.first()
+                        // get all values
+                        dayValue = dayFlow.first()
+                        waterDrunkValue = waterDrunkFlow.first()
+                        onboardingStatusValue = onBoardingStatusFlow.first()
+                        waterGoalValue = waterGoalFlow.first()
                         currentStreakValue = currentStreak.first()
+
+                        // if it is a new day
                         if (dayValue < today) {
                             store.saveDay(today)
+                            // if the water goal was met
                             if (waterDrunkValue >= waterGoalValue) {
                                 store.saveStreak(currentStreakValue + 1)
                             } else {
                                 store.saveStreak(0)
                             }
+                            // reset the water for the new day
                             store.setTodayWater(0)
                         }
                    }
-                    if (onboardingStatus) {
+                    if (onboardingStatusValue) {
                         Layout(waterGoalValue, waterDrunkValue, currentStreakValue, store)
                     } else {
                         OnboardingScreen()
@@ -116,55 +103,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Project3pyuanTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
-fun LayoutPreview() {
-    Project3pyuanTheme {
-        Layout(0, 0, 0, UserStore(LocalContext.current))
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Layout(waterGoal: Int, waterDrunkPassed: Int, currentStreakValue: Int, store: UserStore) {
     Project3pyuanTheme {
-        val context = LocalContext.current
         var waterDrunk by remember { mutableStateOf(waterDrunkPassed) }
         var percentageToGoal by remember { mutableStateOf(min(1.0F, waterDrunk.toFloat()/waterGoal)) }
 
         waterDrunk = (store.getTodayWater.collectAsState(initial = 0)).value
         percentageToGoal = min(1.0F, waterDrunk.toFloat()/waterGoal)
-
-        // Get local density from composable
-        val localDensity = LocalDensity.current
-
-        // Create element height in pixel state
-        var columnHeightPx by remember {
-            mutableStateOf(0f)
-        }
-
-        // Create element height in dp state
-        var columnHeightDp by remember {
-            mutableStateOf(0.dp)
-        }
-
 
         Column(
             modifier = Modifier
@@ -187,11 +134,6 @@ fun Layout(waterGoal: Int, waterDrunkPassed: Int, currentStreakValue: Int, store
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .onGloballyPositioned { coordinates ->
-                        // Set column height using the LayoutCoordinates
-                        columnHeightPx = coordinates.size.height.toFloat()
-                        columnHeightDp = with(localDensity) { coordinates.size.height.toDp() }
-                    }
             ) {
                 val animatedProgress by animateFloatAsState(
                     targetValue = percentageToGoal,
