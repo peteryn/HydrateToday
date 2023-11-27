eb58290eb0a4a566406ed96f5aee846260435620
Peter Yuan <peter.yuan18@gmail.com>
2023-11-26 20:55:46 -0500

    Add streak

--- app/src/main/java/com/example/project3_pyuan/MainActivity.kt
+++ app/src/main/java/com/example/project3_pyuan/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.project3_pyuan
 
-import android.health.connect.datatypes.units.Percentage
 import android.os.Bundle
-import android.util.Log
-import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,9 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.text.style.TextAlign
@@ -49,7 +47,7 @@ import java.lang.Float.min
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val debug = false
+        val debug = true
         setContent {
             Project3pyuanTheme {
                 // A surface container using the 'background' color from the theme
@@ -74,7 +72,7 @@ class MainActivity : ComponentActivity() {
 
                     val currentStreak = store.getCurrentStreak
                     var currentStreakValue: Int
-
+                    var updateStreak: Boolean = true
                     runBlocking(Dispatchers.IO) {
                         // get all values
                         dayValue = dayFlow.first()
@@ -88,16 +86,20 @@ class MainActivity : ComponentActivity() {
                             store.saveDay(today)
                             // if the water goal was met
                             if (waterDrunkValue >= waterGoalValue) {
-                                store.saveStreak(currentStreakValue + 1)
+//                                store.saveStreak(currentStreakValue + 1)
                             } else {
                                 store.saveStreak(0)
                             }
                             // reset the water for the new day
                             store.setTodayWater(0)
+                        } else {
+                            if (onboardingStatusValue) {
+                                updateStreak = false
+                            }
                         }
                    }
                     if (onboardingStatusValue) {
-                        Layout(waterGoalValue, waterDrunkValue, currentStreakValue, store, debug)
+                        Layout(waterGoalValue, waterDrunkValue, currentStreakValue, store, debug, updateStreak)
                     } else {
                         OnboardingScreen()
                     }
@@ -111,15 +113,18 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Layout(waterGoal: Int, waterDrunkPassed: Int, currentStreakValue: Int, store: UserStore, debug: Boolean) {
+fun Layout(waterGoal: Int, waterDrunkPassed: Int, currentStreak: Int, store: UserStore, debug: Boolean, updateStreak: Boolean) {
     Project3pyuanTheme {
         // set the state with the values passed in
         var waterDrunk by remember { mutableStateOf(waterDrunkPassed) }
         var percentageToGoal by remember { mutableStateOf(min(1.0F, waterDrunk.toFloat()/waterGoal)) }
+        var currentStreakState by remember { mutableStateOf(currentStreak) }
+        var updateStreakValue by remember { mutableStateOf(updateStreak) }
 
         // make sure state is synchronized
-        waterDrunk = (store.getTodayWater.collectAsState(initial = 0)).value
+        waterDrunk = store.getTodayWater.collectAsState(initial = 0).value
         percentageToGoal = min(1.0F, waterDrunk.toFloat()/waterGoal)
+        currentStreakState = store.getCurrentStreak.collectAsState(initial = 0).value
 
         Column() {
             Column(modifier = Modifier.weight(1f)) {
@@ -143,6 +148,22 @@ fun Layout(waterGoal: Int, waterDrunkPassed: Int, currentStreakValue: Int, store
                         }) {
                             Text(text = "Reset App")
                         }
+                    }
+                    Box (
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(30.dp)
+                            .align(Alignment.TopEnd)
+                            // info about circle borders from here
+                            // https://stackoverflow.com/questions/66014834/how-to-draw-a-circular-image-in-android-jetpack-compose
+                            .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                            .clip(CircleShape)
+                    ) {
+                        Text (
+                            text = "$currentStreakState",
+                            fontSize = 15.sp,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
                     }
                     Text(
                         text = "$waterGoal oz",
@@ -171,6 +192,13 @@ fun Layout(waterGoal: Int, waterDrunkPassed: Int, currentStreakValue: Int, store
                 val updateWater: (Int) -> Unit = { itemAmountOfWater ->
                     waterDrunk += itemAmountOfWater
                     percentageToGoal = min(1.0F, waterDrunk.toFloat() / waterGoal)
+                    if (waterDrunk >= waterGoal && updateStreakValue) {
+                        currentStreakState++
+                        CoroutineScope(Dispatchers.IO).launch {
+                            store.saveStreak(currentStreakState)
+                        }
+                        updateStreakValue = false
+                    }
                 }
                 // left column
                 Column(modifier = Modifier.weight(1f)) {
